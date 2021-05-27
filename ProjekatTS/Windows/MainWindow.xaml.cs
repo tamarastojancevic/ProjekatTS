d71ebf5426daeeb0e66587b50737640c9849b0d8
Tamara <tamarastojancevic@gmail.com>
2021-05-27 22:50:47 +0200

    Popravka greskica.

---
+++ ProjekatTS/Windows/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjekatTS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+
+        public static implicit operator MainWindow(Windows.Pocetna v)
+        {
+            throw new NotImplementedException();
+        }
+        private void Button2(object sender, RoutedEventArgs e)
+        {
+            Windows.Pocetna pocetna = new Windows.Pocetna();
+            Window.GetWindow(this).Close();
+            pocetna.ShowDialog();
+
+        }
+        private void Button1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+    }
+}
---
+++ ProjekatTS/Windows/Pocetna.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ProjekatTS.Windows
+{
+    /// <summary>
+    /// Interaction logic for Pocetna.xaml
+    /// </summary>
+    public partial class Pocetna : Window
+    {
+        public Pocetna()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        internal static void ShowDialogue()
+        {
+            throw new NotImplementedException();
+        }
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+
+
+            switch (index)
+            {
+                case 0:
+                    GridUserControl.Children.Clear();
+                    GridUserControl.Children.Add(new Windows.Pocetna1());
+                    break;
+                case 1:
+                    GridUserControl.Children.Clear();
+                    GridUserControl.Children.Add(new Windows.Klijenti());
+                    break;
+
+                case 2:
+                    GridUserControl.Children.Clear();
+                    GridUserControl.Children.Add(new Windows.Zahtev());
+                    break;
+
+                case 3:
+                    GridUserControl.Children.Clear();
+                    GridUserControl.Children.Add(new Windows.Evidencija());
+                    break;
+
+                case 4:
+                    GridUserControl.Children.Clear();
+
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MenuItem_ImageFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+    }
+}
